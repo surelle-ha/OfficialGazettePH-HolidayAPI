3f92ae6b8d92b70e60db30bf7e7a38d4280d30ed
Harold <75729237+surelle-ha@users.noreply.github.com>
2024-04-01 12:02:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_holidays(year=datetime.now().year):
         return jsonify({'error': 'Failed to resolve domain name'})
 
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0'
     }
     
     try:
